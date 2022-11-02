1cf10bad783d32905a3b133a9f75cef5362f85e1
melinaeee <86803532+melinaeee@users.noreply.github.com>
2022-11-02 11:40:06 -0300

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Melinae and is{" "}
+        <a href="https://github.com/melinaeee/search-weather" target="_blank">
+          open sourced
+        </a>
+      </footer>
     </div>
   );
 }
