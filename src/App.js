84475988d3fd5d4f0ecd6e37bf5f2266f6ee90ea
melinaeee <86803532+melinaeee@users.noreply.github.com>
2022-11-02 13:43:21 -0300

    Built the html part

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Melinae and is{" "}
-        <a href="https://github.com/melinaeee/search-weather" rel="noopener">
-          open sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather /> 
+        <footer>
+          This project was coded by Melinae and is{" "}
+          <a href="https://github.com/melinaeee/search-weather" rel="noopener">
+            open sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
